420deafae19f83c91a44c7acd27f5272bb278add
SConaway <bigboy.steven.08@me.com>
2016-11-07 08:39:44 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ DEVICE_LIST_ENDPOINT = "api/UserDeviceDetails"
 DEVICE_SET_ENDPOINT = "Device/setDeviceAttribute"
 DEVICE_STATUS_ENDPOINT = "Device/getDeviceAttribute"
 
-USERNAME = "<MYQ_LOGIN_USERNAME>"
-PASSWORD = "<MYQ_LOGIN_PASSWORD>"
+# Example
+#USERNAME = "test%40test.com"
+#PASSWORD = "Passw0rd"
+USERNAME = "<insert username in a url-encoded format>"
+PASSWORD = "<insert password in a url-encoded format>"
 
 myq_userid                  = ""
 myq_security_token          = ""
@@ -21,8 +24,9 @@ myq_cached_login_response   = ""
 myq_device_id               = ""
 
 def lambda_handler(event, context):
-
-    if event['session']['application']['applicationId'] != "amzn1.ask.skill.<your-alexa-skills-id>":
+# Example
+# if event['session']['application']['applicationId'] != "amzn1.ask.skill.f81avc7f-3c7f-467f-8ce7-41c7a10fc8w3":
+    if event['session']['application']['applicationId'] != "<insert skill id>":
         print "Invalid Application ID"
         raise
     else:
@@ -57,6 +61,7 @@ def close():
 def status():
     response = check_door_state()
     state = response["AttributeValue"]
+
     if state == "9":
         return "open"
     elif state == "2":
@@ -65,6 +70,10 @@ def status():
         return "opening"
     elif state == "5":
         return "closing"
+    elif state == "1":
+        return "open"
+    elif state == "4":
+        return "opening"
     else:
         return state + " is an unknown state for the door."
 
